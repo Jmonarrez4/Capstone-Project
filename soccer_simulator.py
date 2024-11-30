d01d2784f9fe0e0f6d79a382c7501717e1ec9f12
Juan Monarrez <Juan.monarrez728@gmail.com>
2024-11-29 18:47:05 -0600

    Updated Code
    Updated code to adhere to pylint recommendations.

--- soccer_simulator.py
+++ soccer_simulator.py
@@ -1,5 +1,6 @@
 """This is a Soccer Team Simulator that implements random scores, tactics, training methods,
-and morale to help the user find success! This program includes a promotion/relegation system as well."""
+and morale to help the user find success!
+This program includes a promotion/relegation system as well."""
 
 # Import the random library to use in the code.
 # Nicknaming rn to make calling functions of the library easy.
@@ -9,6 +10,7 @@ import random as rn
 # This will help keep track of wins, losses, ties.
 # The overall points and goal difference will break up any ties in the final rankings.
 class Team:
+    """Determines what a team is and its attributes."""
     def __init__(self, name):
         self.name = name
         self.points = 0
@@ -20,7 +22,8 @@ class Team:
         self.training_focus = None # Default training focus
         self.morale_boost = 'neutral' # Default morale boost
 
-    def reset(self): # Resets the record, and team attributes after each season.
+    def reset(self):
+        """Resets the record, and team attributes after each season."""
         self.points = 0
         self.goal_difference = 0
         self.wins = 0
@@ -33,16 +36,18 @@ class Team:
     # This is how the final standings will apper to the user.
     # The order is name: points, goal difference, wins, losses, ties.
     def __repr__(self):
-        return (f"{self.name}: {self.points} pts, GD: {self.goal_difference}, W: {self.wins}, L: {self.losses}, "
-                f"T: {self.ties}")
+        return (f"{self.name}: {self.points} pts, GD: {self.goal_difference}, "
+                f"W: {self.wins}, L: {self.losses}, T: {self.ties}")
 
 # Define PostGameOptions to add more variety to the user interface
-class PostGameOptions: # These will be the options the user has to affect their team.
+class PostGameOptions:
+    """These will be the options the user has to affect their team."""
     def __init__(self, user, team):
         self.user = user
         self.team = team
 
-    def display_options(self): # Shows the user the options they have.
+    def display_options(self):
+        """Shows the user the options they have."""
         self.show_team_record()
         print(f"\n{self.user}, choose an option to influence {self.team.name}:")
         print("1. Change Tactics")
@@ -50,7 +55,8 @@ class PostGameOptions: # These will be the options the user has to affect their 
         print("3. Boost Team Morale")
         print("4. Don't Change Anything")
 
-    def execute_option(self, choice): # Implements the choice of the user.
+    def execute_option(self, choice):
+        """Implements the choice of the user."""
         if choice == 1:
             self.change_tactics()
         elif choice == 2:
@@ -62,23 +68,26 @@ class PostGameOptions: # These will be the options the user has to affect their 
         else:
             print("Invalid choice. Please select again.")
 
-    def show_team_record(self): # Shows the user their current record in order to help their decision.
+    def show_team_record(self):
+        """Shows the user their current record in order to help their decision."""
         print(f"Current record for {self.team.name}:")
         print(f"Wins: {self.team.wins}, Losses: {self.team.losses}, Ties: {self.team.ties}")
         print(f"Current tactic is {self.team.current_tactic}")
         print(f"Current training focus is {self.team.training_focus}")
         print(f"Current morale booster is {self.team.morale_boost}")
 
-    def change_tactics(self): # Implements the tactic change option.
+    def change_tactics(self):
+        """Implements the tactic change option."""
         print("You have chosen to change tactics.")
-        tactic_option = input("What tactic would you like to use?: (offensive, balanced, defensive)")
+        tactic_option = input("Which tactic will you use?: (offensive, balanced, defensive)")
         if tactic_option in ['offensive', 'balanced', 'defensive']:
             self.team.current_tactic = tactic_option
             print(f"Your team will play {tactic_option} next game!")
         else:
             print("You have not chosen a valid option, choose again")
 
-    def adjust_training(self): # Implements the training focus adjustment.
+    def adjust_training(self):
+        """Implements the training focus adjustment."""
         print("You have chosen to adjust player training")
         practice_option = input("What would you like to change in practice?: (offense, defense)")
         if practice_option in ['offense', 'defense']:
@@ -87,7 +96,8 @@ class PostGameOptions: # These will be the options the user has to affect their 
         else:
             print("You have not chosen a valid option, choose again")
 
-    def boost_morale(self): # Implements the morale booster.
+    def boost_morale(self):
+        """Implements the morale booster."""
         print("You have chosen to boost team morale")
         morale_booster = input("How will you boost team morale?: (positive, negative)")
         if morale_booster in ['positive', 'negative']:
@@ -97,13 +107,15 @@ class PostGameOptions: # These will be the options the user has to affect their 
             print("You have not chosen a valid option, choose again")
 
     @staticmethod
-    def advance_schedule(): # Advances to the next game with no effect on the actual score.
+    def advance_schedule():
+        """Advances to the next game with no effect on the actual score."""
         print("You have chosen not to change anything! Moving on to next week")
 
 # This is the function used to simulate the regular season.
 # Every team will play every other team at least once.
 # Top Range of goals in games tend to be 5
 def play_match(team1, team2, game_number=1):
+    """Regular season game."""
 
     if game_number % 4 == 0: # Resets tactics after 4 games
         team1.current_tactic = 'balanced'
@@ -188,6 +200,7 @@ def play_match(team1, team2, game_number=1):
 # Penalty shootouts occur when both teams end extra time still tied.
 # Regular season games do not end in penalties as teams can tie in the regular season.
 def penalties(team1, team2):
+    """Helps to find a winner after extra time has been completed."""
     # Simulate penalty shootout
     team1_goals = rn.randint(0, 11)
     team2_goals = rn.randint(0, 11)
@@ -203,8 +216,8 @@ def penalties(team1, team2):
 # Extra time occurs when both teams end regular time with the same amount of goals.
 # Regular season games do not go into extra time as teams can tie in the regular season.
 # Extra time consists of 2 shorter halves therefore the max range has been lowered from 3 ot 1.
-# The chance of a goal being scored in extra time is lower due to the shorter time and team exhaustion.
 def extra_time(team1, team2):
+    """Helps to find a winner after regular time has been completed."""
     # Simulate extra time and determine if penalties are needed
     team1_goals = rn.randint(0, 1)
     team2_goals = rn.randint(0, 1)
@@ -222,6 +235,7 @@ def extra_time(team1, team2):
 # Playoff matches can not end in a tie, therefore this will determine if teams go into extra time.
 # Playoff games tend to be more intense and skilled therefore we lower the max range from 5 to 3.
 def playoff_match(team1, team2):
+    """Simulates a game with playoff rules."""
     # Simulate a playoff match with extra time and penalties
     team1_goals = rn.randint(0, 3)
     team2_goals = rn.randint(0, 3)
@@ -229,38 +243,42 @@ def playoff_match(team1, team2):
     if team1_goals > team2_goals:
         team1.wins += 1
         team2.losses += 1
-        print(f"The {team1.name} wins the match against the {team2.name} with a score of {team1_goals} to "
-              f"{team2_goals}.")
+        print(f"The {team1.name} wins the match against the {team2.name} "
+              f"with a score of {team1_goals} to {team2_goals}.")
         return team1
     elif team2_goals > team1_goals:
         team2.wins += 1
         team1.losses += 1
-        print(f"The {team2.name} wins the match against the {team1.name} with a score of {team2_goals} to "
-              f"{team1_goals}.")
+        print(f"The {team2.name} wins the match against the {team1.name} "
+              f"with a score of {team2_goals} to {team1_goals}.")
         return team2
     else:
         winner = extra_time(team1, team2)
-        print(f"The match between {team1.name} and {team2.name} ended in a tie after regular time. Winner after "
-              f"extra time or penalties: {winner.name}")
+        print(f"The match between {team1.name} and {team2.name} ended in a tie after regular time. "
+              f"Winner after extra time or penalties: {winner.name}")
 
     return winner
 
 def simulate_season(teams, user, user_team_name):
+    """Goes through list of teams and pairs them together to create a record for each team."""
     # Simulate the regular season
     for team in teams:
         team.reset()
 
-    for i in range(len(teams)):
+    number_of_teams = len(teams)
+    for i in range(number_of_teams):
         for j in range(i + 1, len(teams)):
             play_match(teams[i], teams[j])
             # Add post-game options for the user's team
-            if teams[i].name == user_team_name or teams[j].name == user_team_name:
-                post_game = PostGameOptions(user, teams[i] if teams[i].name == user_team_name else teams[j])
+            if user_team_name in (teams[i].name, teams[j].name):
+                post_game = PostGameOptions(user,
+                        teams[i] if teams[i].name == user_team_name else teams[j])
                 post_game.display_options()
                 choice = int(input("Enter your choice: "))
                 post_game.execute_option(choice)
 
 def main():
+    """Main function part of code."""
     # Asks the user for their name.
     user = input("What is your name? ")
     user_team_name = input("What is your team name? ")
@@ -272,7 +290,8 @@ def main():
     # Names of teams in league.
     # Allows user to create a team and add them to the league in hopes of winning it all.
     teams = [Team("Kansas City Legends"), Team("Pittsburgh Iron-men"),
-             Team("Atlanta Flames"), Team("Los Angeles Stars"), Team("Detroit Pride"), Team("Phoenix Coyotes"),
+             Team("Atlanta Flames"), Team("Los Angeles Stars"),
+             Team("Detroit Pride"), Team("Phoenix Coyotes"),
              Team("San Fransisco Miners"), Team("Miami Manatees"), Team("New England Liberty"),
              Team("Dallas Stampede"), Team("Oklahoma City Cyclones"), Team("New York Skyscrapers"),
              Team("St. Louis Steamboats"), Team("Minnesota Blizzard"), Team("Washington Storm"),
@@ -283,13 +302,17 @@ def main():
              Team("Albuquerque Indians"), Team("Hawaii Volcanoes"), Team("Vancouver Whalers"),
              Team("Toronto Lumberjacks"), Team(user_team_name)]
 
-    division_two_teams = [Team("Austin Rough riders"), Team("New York Metros"), Team("Tampa Bay Speedsters"),
-                          Team("Montreal Underground"), Team("Connecticut Robins"), Team("San Diego Surfers"),
-                          Team("Green Bay Lake Monsters"), Team("San Antonio Cavalry"), Team("New Orleans Voodoo"),
-                          Team("Memphis Jazz"), Team("Nashville Rockstars"), Team("Cincinnati Gorillas"),
-                          Team("Indianapolis Rush"), Team("Buffalo Waterfalls"), Team("Boston Fishermen"),
-                          Team("Cleveland Community"), Team("Manhattan Prairie Dogs"), Team("Des Moine Shockers"),
-                          Team("Denver Altitude"), Team("Montana Stallions")]
+    division_two_teams = [Team("Austin Rough riders"),
+                          Team("New York Metros"), Team("Tampa Bay Speedsters"),
+                          Team("Montreal Underground"), Team("Connecticut Robins"),
+                          Team("San Diego Surfers"), Team("Green Bay Lake Monsters"),
+                          Team("San Antonio Cavalry"), Team("New Orleans Voodoo"),
+                          Team("Memphis Jazz"), Team("Nashville Rockstars"),
+                          Team("Cincinnati Gorillas"), Team("Indianapolis Rush"),
+                          Team("Buffalo Waterfalls"), Team("Boston Fishermen"),
+                          Team("Cleveland Community"), Team("Manhattan Prairie Dogs"),
+                          Team("Des Moine Shockers"), Team("Denver Altitude"),
+                          Team("Montana Stallions")]
 
     year = 2024
 
@@ -311,7 +334,8 @@ def main():
 
         # Simulate playoffs.
         print("\nPlayoffs:")
-        quarter_finalists = [playoff_teams[0], playoff_teams[1], playoff_teams[2], playoff_teams[3], playoff_teams[4],
+        quarter_finalists = [playoff_teams[0], playoff_teams[1], playoff_teams[2],
+                             playoff_teams[3], playoff_teams[4],
                              playoff_teams[5], playoff_teams[6], playoff_teams[7]]
 
         # Quarter-finals.
@@ -343,13 +367,16 @@ def main():
         print("\nDivision Two season standings:")
         simulate_season(division_two_teams, user, user_team_name)
         position = 1
-        for team in sorted(division_two_teams, key=lambda x: (x.points, x.goal_difference), reverse=True):
+        for team in sorted(division_two_teams,
+                           key=lambda x: (x.points, x.goal_difference), reverse=True):
             print(position, team)
             position += 1
 
-        relegated_teams = sorted(teams, key=lambda x: (x.points, x.goal_difference), reverse=True)
+        relegated_teams = sorted(teams,
+                                 key=lambda x: (x.points, x.goal_difference), reverse=True)
         relegated = relegated_teams[-3:]
-        promoted_teams = sorted(division_two_teams, key=lambda x: (x.points, x.goal_difference), reverse=True)
+        promoted_teams = sorted(division_two_teams,
+                                key=lambda x: (x.points, x.goal_difference), reverse=True)
         promoted = promoted_teams[:3]
 
         print(f"\nTeams relegated to Division Two: {relegated[0].name}, {relegated[1].name}, "
